6f90f2ec9de9ea75613944b4e62b61ba50025071
Nathan Ladd <nathan@obsidianexchange.com>
2016-08-27 13:03:45 -0500

    Benchmark against stdlib queue is added

---
+++ benchmarks/benchmark_init.rb
@@ -0,0 +1,4 @@
+require_relative '../init'
+
+require 'benchmark'
+require 'benchmark/ips'
---
+++ benchmarks/queue.rb
@@ -0,0 +1,26 @@
+require_relative './benchmark_init'
+
+Benchmark.ips do |bm|
+  object = Object.new
+
+  bm.report 'stdlib-queue' do |iterations|
+    queue = Queue.new
+
+    iterations.times do
+      queue.enq object
+      queue.deq
+    end
+  end
+
+  bm.report 'actor-queue' do |iterations|
+    actor_queue = Actor::Queue.new
+    actor_queue.consumer_started
+
+    iterations.times do |position| 
+      actor_queue.write object
+      actor_queue.read position, block: true
+    end
+  end
+
+  bm.compare!
+end
