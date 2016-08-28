93d00d4fdef77014e53c692381553c981528dab1
Nathan Ladd <nathan@obsidianexchange.com>
2016-08-28 12:27:22 -0500

    Remove unnecessary begin/end block

--- lib/actor/handle.rb
+++ lib/actor/handle.rb
@@ -13,27 +13,25 @@ module Actor
       def handle message
         super
 
-        begin
-          case message
-          when Messaging::SystemMessage::Pause then
-            self.actor_state = State::Paused
+        case message
+        when Messaging::SystemMessage::Pause then
+          self.actor_state = State::Paused
 
-          when Messaging::SystemMessage::Resume then
-            self.actor_state = State::Running
+        when Messaging::SystemMessage::Resume then
+          self.actor_state = State::Running
 
-          when Messaging::SystemMessage::Stop then
-            self.actor_state = State::Stopped
-            raise StopIteration
+        when Messaging::SystemMessage::Stop then
+          self.actor_state = State::Stopped
+          raise StopIteration
 
-          when Messaging::SystemMessage::RecordStatus then
-            status = message.status
+        when Messaging::SystemMessage::RecordStatus then
+          status = message.status
 
-            Statistics::Copy.(status, actor_statistics)
+          Statistics::Copy.(status, actor_statistics)
 
-            status.state = actor_state
+          status.state = actor_state
 
-            Messaging::Writer.write status, message.reply_address
-          end
+          Messaging::Writer.write status, message.reply_address
         end
       end
     end
