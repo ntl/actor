008d0fcab5aaa828361f893168c29aa591bcf9f0
Nathan Ladd <nathan@obsidianexchange.com>
2016-11-11 12:57:43 -0600

    Test is reworked to use fixture

--- test/scripts/supervisor/non_default_thread_group.rb
+++ test/scripts/supervisor/non_default_thread_group.rb
@@ -5,16 +5,12 @@ context "Supervisor is Constructed Within a Non-Default Thread Group" do
 
   thread_group = ThreadGroup.new
 
-  thread = Thread.new do
+  supervisor = Fixtures::ExecuteWithinThread.() do
     thread_group.add Thread.current
-    supervisor = Build.(Supervisor)
-    sleep
+    Build.(Supervisor)
   end
-  Thread.pass until thread.stop?
 
   test "Thread group is set to that of thread in which supervisor is built" do
     assert supervisor.thread_group == thread_group
   end
-
-  thread.kill
 end
