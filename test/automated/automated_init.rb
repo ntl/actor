782f5fd56f8086978f15ae84d2ba4724fcdd8f93
Nathan Ladd <nathanladd+github@gmail.com>
2018-04-15 14:57:53 -0500

    Automatically fail tests if threads remain

--- test/automated/automated_init.rb
+++ test/automated/automated_init.rb
@@ -1,1 +1,13 @@
+ThreadGroup::Default.enclose
+
 require_relative '../test_init'
+
+at_exit do
+  orphaned_threads = ThreadGroup::Default.list.reject do |thread|
+    thread == ::Thread.main
+  end
+
+  if orphaned_threads.any?
+    fail "One or more threads were not stopped (Count: #{orphaned_threads.count})"
+  end
+end
