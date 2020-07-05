59a4a4adf9fff042b9093c04213e5a47c77c8445
Nathan Ladd <nathanladd@gmail.com>
2020-07-04 22:17:29 -0500

    bin/setup is removed

--- test/automated.rb
+++ test/automated.rb
@@ -1,9 +1,7 @@
 #!/usr/bin/env ruby --disable-gems
 
-ENV['TEST_BENCH_TESTS_DIR'] ||= 'test/scripts'
-
 require_relative './test_init'
 
 require 'test_bench/cli'
 
-TestBench::CLI.() or exit 1
+TestBench::CLI.('test/automated') or exit 1
